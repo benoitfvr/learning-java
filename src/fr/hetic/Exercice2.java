da6155b1da36626b66d43cb5d9d5c3693ae189c3
Benoît Favrie <benoit14.favrie@gmail.com>
2024-04-10 10:03:16 +0200

    Feat(exercice 2+): Now can read all files of a folder (and sub folders)

--- src/fr/hetic/Exercice2.java
+++ src/fr/hetic/Exercice2.java
@@ -1,34 +1,59 @@
 package fr.hetic;
+
 import java.io.*;
 
 public class Exercice2 {
-    public static void main(String[] args) {
-        String inputFilePath = args[0];
-        String outputFilePath = args[1];
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
-             FileWriter writer = new FileWriter(outputFilePath)) {
-            
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar Exercice2.jar <input directory>");
+            System.exit(1);
+        }
+
+        String inputDirectoryPath = args[0];
+        File inputDirectory = new File(inputDirectoryPath);
+
+        processDirectory(inputDirectory);
+    }
+
+    private static void processDirectory(File directory) {
+        File[] files = directory.listFiles(file -> file.isDirectory() || file.getName().endsWith(".op"));
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    processDirectory(file);
+                } else {
+                    String outputFilePath = file.getPath().replace(".op", ".res");
+                    processFile(file, new File(outputFilePath));
+                }
+            }
+        }
+    }
+
+    private static void processFile(File inputFile, File outputFile) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] tokens = line.split(" ");
-                int a = Integer.parseInt(tokens[0]);
-                int b = Integer.parseInt(tokens[1]);
-                String operationSymbol = tokens[2];
-
                 try {
+                    String[] tokens = line.split(" ");
+                    int a = Integer.parseInt(tokens[0]);
+                    int b = Integer.parseInt(tokens[1]);
+                    String operationSymbol = tokens[2];
+
                     Operation operation = OperationFactory.getOperation(operationSymbol);
                     int result = operation.execute(a, b);
                     writer.write(String.valueOf(result));
-                    writer.write(System.lineSeparator());
+                    writer.newLine();
                 } catch (IllegalArgumentException | ArithmeticException e) {
                     writer.write("Error: " + e.getMessage());
-                    writer.write(System.lineSeparator());
+                    writer.newLine();
                 }
             }
         } catch (IOException e) {
-            System.out.println("An error occurred: " + e.getMessage());
-            System.exit(1);
+            System.out.println("An error occurred processing file " + inputFile.getPath() + ": " + e.getMessage());
         }
     }
 }
